ce74edcba3de6ec79a1b7eda6f80f6d325a1ebe4
Angad <angadbhatti18@gmail.com>
2022-03-08 20:00:45 -0800

    sending index and show data

---
+++ views/index.jsx
@@ -0,0 +1,26 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads})  {
+  return (
+    <Default>
+      <h2>Index Page</h2>
+      <p>I have {breads[0].name} bread!</p>
+      
+      {
+        breads.map((bread, index)=> {
+          return (
+            <li key={index}>
+              <a href={`/breads/${index}`}>
+                {bread.name}
+              </a>
+            </li>
+            )
+          })
+      }
+      
+    </Default>
+  )
+}
+
+module.exports = Index
